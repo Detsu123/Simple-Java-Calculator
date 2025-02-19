7aea5693323d1d0a80caea7968d9557e45380895
Detsu123- <deegiiurnaa247@gmail.com>
2025-02-19 22:58:04 +0800

    Update Calculator.java

--- src/simplejavacalculator/Calculator.java
+++ src/simplejavacalculator/Calculator.java
@@ -1,65 +1,34 @@
-/**
- * @name        Simple Java Calculator
- * @package     ph.calculator
- * @file        Main.java
- * @author      SORIA Pierre-Henry
- * @email       pierrehs@hotmail.com
- * @link        http://github.com/pH-7
- * @copyright   Copyright Pierre-Henry SORIA, All Rights Reserved.
- * @license     Apache (http://www.apache.org/licenses/LICENSE-2.0)
- */
-
 package simplejavacalculator;
 
 import static java.lang.Double.NaN;
-import static java.lang.Math.log;
-import static java.lang.Math.log10;
-import static java.lang.Math.pow;
+import static java.lang.Math.*;
 
 public class Calculator {
 
     public enum BiOperatorModes {
-        normal, add, minus, multiply, divide , xpowerofy 
+        ADD, MINUS, MULTIPLY, DIVIDE, X_POWER_OF_Y
     }
 
     public enum MonoOperatorModes {
-        square, squareRoot, oneDividedBy, cos, sin, tan, log, rate, abs, ln,
+        SQUARE, SQUARE_ROOT, ONE_DIVIDED_BY, COS, SIN, TAN, LOG, LN, RATE, ABS
     }
 
     private Double num1, num2;
-    private BiOperatorModes mode = BiOperatorModes.normal;
+    private BiOperatorModes mode = null;
 
     private Double calculateBiImpl() {
-        if (mode.equals(BiOperatorModes.normal)) {
-            return num2;
+        switch (mode) {
+            case ADD: return num1 + num2;
+            case MINUS: return num1 - num2;
+            case MULTIPLY: return num1 * num2;
+            case DIVIDE: return num2 == 0 ? NaN : num1 / num2;
+            case X_POWER_OF_Y: return pow(num1, num2);
+            default: return NaN;
         }
-        if (mode.equals(BiOperatorModes.add)) {
-            if (num2 != 0) {
-                return num1 + num2;
-            }
-
-            return num1;
-        }
-        if (mode.equals(BiOperatorModes.minus)) {
-            return num1 - num2;
-        }
-        if (mode.equals(BiOperatorModes.multiply)) {
-            return num1 * num2;
-        }
-        if (mode.equals(BiOperatorModes.divide)) {
-            return num1 / num2;
-        }
-        if (mode.equals(BiOperatorModes.xpowerofy)) {
-            return pow(num1,num2);
-        }
-
-        // never reach
-        throw new Error();
     }
 
     public Double calculateBi(BiOperatorModes newMode, Double num) {
-        if (mode.equals(BiOperatorModes.normal)) {
-            num2 = 0.0;
+        if (mode == null) {
             num1 = num;
             mode = newMode;
             return NaN;
@@ -72,60 +41,29 @@ public class Calculator {
     }
 
     public Double calculateEqual(Double num) {
-        return calculateBi(BiOperatorModes.normal, num);
+        return calculateBi(null, num);
     }
 
     public Double reset() {
-        num2 = 0.0;
-        num1 = 0.0;
-        mode = BiOperatorModes.normal;
-
+        num1 = null;
+        num2 = null;
+        mode = null;
         return NaN;
     }
 
-    
     public Double calculateMono(MonoOperatorModes newMode, Double num) {
-        if (newMode.equals(MonoOperatorModes.square)) {
-            return num * num;
+        switch (newMode) {
+            case SQUARE: return num * num;
+            case SQUARE_ROOT: return sqrt(num);
+            case ONE_DIVIDED_BY: return num == 0 ? NaN : 1 / num;
+            case COS: return cos(toRadians(num));
+            case SIN: return sin(toRadians(num));
+            case TAN: return tan(toRadians(num));
+            case LOG: return log10(num);
+            case LN: return log(num);
+            case RATE: return num / 100;
+            case ABS: return abs(num);
+            default: throw new IllegalArgumentException("Invalid Mono Operator");
         }
-        if (newMode.equals(MonoOperatorModes.squareRoot)) {
-            return Math.sqrt(num);
-        }
-        if (newMode.equals(MonoOperatorModes.oneDividedBy)) {
-            return 1 / num;
-        }
-        if (newMode.equals(MonoOperatorModes.cos)) {
-            return Math.cos(Math.toRadians(num));
-        }
-        if (newMode.equals(MonoOperatorModes.sin)) {
-            return Math.sin(Math.toRadians(num));
-        }
-        if (newMode.equals(MonoOperatorModes.tan)) {
-            if (num == 0 || num % 180 == 0 ) {
-                return 0.0;
-            }
-            if (num % 90 == 0.0 && num % 180 != 0.0) {
-                return NaN;
-            }
-
-            return Math.tan(Math.toRadians(num));
-        }
-        if (newMode.equals(MonoOperatorModes.log)) {
-            return log10(num);
-        }
-        if (newMode.equals(MonoOperatorModes.ln)) {
-            return log(num);
-        }
-        if (newMode.equals(MonoOperatorModes.rate) ) {
-            return num / 100;
-        }
-        if (newMode.equals(MonoOperatorModes.abs)){
-            return Math.abs(num);
-        }
-
-        // never reach
-        throw new Error();
     }
-
 }
-
